51384002571d066e86c9839361d8f9c76dd3b378
austiecodes <way24h+gh@gmail.com>
2022-11-15 20:21:21 +0800

    add headbar, home

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="toolbar">
+      <el-dropdown>
+        <el-icon style="margin-right: 8px; margin-top: center; font-size: 20px;"
+          ><setting
+        /></el-icon>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Person Info</el-dropdown-item>
+            <el-dropdown-item>Exit</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <span>Tom</span>
+    </div>
+</template>
+  
+<script setup lang="ts">
+    import { Setting } from '@element-plus/icons-vue'
+</script>
+  
+  <style scoped>
+    .toolbar{
+      margin-top: auto;
+    }
+  </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Navigation from './Navigation.vue'
+import HeaderBar from './HeaderBar.vue';
+</script>
+
+<template>
+
+    <el-container class="layout-container">
+  
+      <el-header style="text-align: right; font-size: 12px">
+        <HeaderBar/>
+      </el-header>
+  
+      <el-container>
+  
+        <el-aside style="height: 100%;">
+          <Navigation/>
+        </el-aside>
+  
+        <el-main height=100%>
+        </el-main>
+      </el-container>
+  
+    </el-container>
+  </template>
+  
+  
+  <style scoped>
+  .layout-container{
+    height: 100vh;
+  }
+  .layout-container .el-header {
+    position: relative;
+    background-color: var(--el-color-primary-light-7);
+    
+  }
+  .layout-container .el-aside {
+    color: var(--el-text-color-primary);
+  }
+  .layout-container .el-main {
+    padding: 0;
+  }
+  </style>
